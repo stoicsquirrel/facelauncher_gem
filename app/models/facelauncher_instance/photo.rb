7f14480c092d2f749ecd17adf8be89ddfa388c1d
Alex Melman <amelman5@gmail.com>
2012-10-15 19:04:13 -0400

    Added save method to Photo model and multiple bug fixes.

--- app/models/facelauncher_instance/photo.rb
+++ app/models/facelauncher_instance/photo.rb
@@ -110,44 +110,62 @@ module FacelauncherInstance
     end
 
     def self.create(params)
-      # Select only model attributes from params to send as part of our request.
-      params.select!{ |k,v| @@attributes.include?(k) }
+      photo = self.new(params)
+      photo.save
 
-      # If there is a file URL included, then send it off to the server for processing.
-      if params.key?(:file_url)
-        Faraday.new(:url => FacelauncherInstance::Engine.config.server_url) do |conn|
-          conn.request :url_encoded
-          conn.adapter :net_http
-          conn.basic_auth FacelauncherInstance::Engine.config.program_id, FacelauncherInstance::Engine.config.program_access_key
+      return photo
+    end
 
-          response = conn.post("/photos.json", { :photo => params })
-          binding.pry
-          if response.status != 200
-            # TODO: Add errors
+    def self.new(params)
+      if params.key?(:photo)
+        # Select only model attributes from params to send as part of our request.
+        params[:photo].select!{ |k,v| @@attributes.include?(k) }
+      end
+
+      super(params)
+    end
+
+    def save
+      if self.valid?
+        # If there is a file URL included, then send it off to the server for processing.
+        if !self.file_url.nil?
+          Faraday.new(:url => FacelauncherInstance::Engine.config.server_url) do |conn|
+            conn.request :url_encoded
+            conn.adapter :net_http
+            conn.basic_auth FacelauncherInstance::Engine.config.program_id, FacelauncherInstance::Engine.config.program_access_key
+
+            response = conn.post("/photos.json", self.to_json)
+
+            if response.status == 200
+              return true
+              # TODO: Add errors
+            end
           end
-        end
-      # If there is a file of the correct type attached, then save it to the server, otherwise, just return.
-      elsif params.key?(:file) && params[:file].content_type =~ /^image\/(jpeg|gif|png)$/
-        Faraday.new(:url => FacelauncherInstance::Engine.config.server_url) do |conn|
-          conn.request :multipart
-          conn.request :url_encoded
-          conn.adapter :net_http
-          conn.basic_auth FacelauncherInstance::Engine.config.program_id, FacelauncherInstance::Engine.config.program_access_key
+        # URGENT: FILE UPLOADING THROUGH BROWSER NOW NEEDS TO BE RETESTED.
+        # If there is a file of the correct type attached, then save it to the server, otherwise, just return.
+        elsif !self.file.nil? && self.file.content_type =~ /^image\/(jpeg|gif|png)$/
+          Faraday.new(:url => FacelauncherInstance::Engine.config.server_url) do |conn|
+            conn.request :multipart
+            conn.request :url_encoded
+            conn.adapter :net_http
+            conn.basic_auth FacelauncherInstance::Engine.config.program_id, FacelauncherInstance::Engine.config.program_access_key
 
-          FileUtils.mkdir_p("#{Rails.root}/tmp/images/uploaded") # Make the temp directory if one doesn't exist
-          tmp_filename = "#{Rails.root}/tmp/images/uploaded/#{params[:file].original_filename}"
-          FileUtils.copy(params[:file].path, tmp_filename)
-          payload = { :photo => params }
-          payload[:photo][:file] = Faraday::UploadIO.new(tmp_filename, params[:file].content_type)
+            FileUtils.mkdir_p("#{Rails.root}/tmp/images/uploaded") # Make the temp directory if one doesn't exist
+            tmp_filename = "#{Rails.root}/tmp/images/uploaded/#{params[:photo][:file].original_filename}"
+            # FileUtils.copy(params[:photo][:file].path, tmp_filename)
+            payload = params
+            # params[:photo][:file] = Faraday::UploadIO.new(tmp_filename, params[:photo][:file].content_type)
 
-          response = conn.post("/photos.json", payload)
-          if response.status != 200
-            # TODO: Add errors
+            response = conn.post("/photos.json", payload)
+            if response.status == 200
+              return true
+              # TODO: Add errors
+            end
           end
         end
       end
 
-      photo = self.new(params)
+      return false
     end
 
     protected
