998dc8fbb2f9112d2ca04af33c8263a71d87aafe
Alex Melman <amelman5@gmail.com>
2012-10-16 13:27:59 -0400

    Bug fix

--- app/models/facelauncher_instance/photo.rb
+++ app/models/facelauncher_instance/photo.rb
@@ -120,14 +120,16 @@ module FacelauncherInstance
 
     def save
       if self.valid?
+        params = { "photo" => attributes.select { |k,v| @@attributes.include?(k.to_sym) } }
+
         # If there is a file URL included, then send it off to the server for processing.
         if !self.file_url.nil?
           Faraday.new(:url => FacelauncherInstance::Engine.config.server_url) do |conn|
             conn.request :url_encoded
             conn.adapter :net_http
             conn.basic_auth FacelauncherInstance::Engine.config.program_id, FacelauncherInstance::Engine.config.program_access_key
-
-            response = conn.post("/photos.json", self.to_json)
+            binding.pry
+            response = conn.post("/photos.json", params)
 
             if response.status == 200
               return true
@@ -146,10 +148,9 @@ module FacelauncherInstance
             FileUtils.mkdir_p("#{Rails.root}/tmp/images/uploaded") # Make the temp directory if one doesn't exist
             tmp_filename = "#{Rails.root}/tmp/images/uploaded/#{params[:photo][:file].original_filename}"
             # FileUtils.copy(params[:photo][:file].path, tmp_filename)
-            payload = params
             # params[:photo][:file] = Faraday::UploadIO.new(tmp_filename, params[:photo][:file].content_type)
 
-            response = conn.post("/photos.json", payload)
+            response = conn.post("/photos.json", params)
             if response.status == 200
               return true
               # TODO: Add errors
