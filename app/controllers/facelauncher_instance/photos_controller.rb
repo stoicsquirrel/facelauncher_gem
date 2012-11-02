0ea293546923cdc11f8f5b650f58f08d831b4026
Alex Melman <amelman5@gmail.com>
2012-11-02 15:43:15 -0400

    Added JSON output for photos index method.

--- app/controllers/facelauncher_instance/photos_controller.rb
+++ app/controllers/facelauncher_instance/photos_controller.rb
@@ -5,15 +5,22 @@ module FacelauncherInstance
     layout 'application' # Use the application's layout instead of the gem's layout
 
     def index
+      if params.key? :albums || params.key? :photo_album_id
+        @photos = []
+        params[:albums].each do |photo_album_id|
+          @photos += Photo.find_by_photo_album_id(photo_album_id)
+        end
+      else
+        @photos = Photo.all
+      end
+
       respond_to do |format|
-        format.html do
-          if params.key? :albums
-            @photos = []
-            params[:albums].each do |photo_album_id|
-              @photos += Photo.find_by_photo_album_id(photo_album_id)
-            end
+        format.html
+        format.json do
+          if self.respond_to?('render_index_as_json')
+            render_index_as_json
           else
-            @photos = Photo.all
+            render :json, @photos
           end
         end
       end
