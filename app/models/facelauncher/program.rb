a90833e5e94e31bb0b08a165a0163eef94228dec
Alex Melman <amelman5@gmail.com>
2012-11-09 18:05:25 -0500

    Removed app_url from Program model.

--- app/models/facelauncher/program.rb
+++ app/models/facelauncher/program.rb
@@ -4,7 +4,7 @@ module Facelauncher
     cache_expiration(ENV.key?("FACELAUNCHER_PROGRAM_CACHE_EXPIRATION") ? ENV["FACELAUNCHER_PROGRAM_CACHE_EXPIRATION"] : 1.minute)
 
     self.attributes = [
-      :id, :active, :app_url, :name, :short_name, :description,
+      :id, :active, :name, :short_name, :description,
       :additional_info_1, :additional_info_2, :additional_info_3,
       :photos_updated_at, :videos_updated_at, :created_at, :updated_at
     ]
